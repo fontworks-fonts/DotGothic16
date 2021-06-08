e44ca7bb46e7f353302c1431bf752af007c4fdfe
Aaron <aaron@sajatypeworks.com>
2021-06-08 09:41:49 -0700

    Hinting removed
    Per GF request, hinting removed. Fonts rebuilt.

--- build.py
+++ build.py
@@ -7,6 +7,13 @@ from pathlib import Path
 print ("[DotGothic16] Generating TTFs")
 __main__.main(("-g","sources/DotGothic16.glyphs", "-o","ttf",))
 
+def GASP_set(font:TTFont):
+    if "gasp" not in font:
+        font["gasp"] = newTable("gasp")
+        font["gasp"].gaspRange = {}
+    if font["gasp"].gaspRange != {65535: 0x000A}:
+        font["gasp"].gaspRange = {65535: 0x000A}
+
 for font in Path("master_ttf").glob("*.ttf"):
     modifiedFont = TTFont(font)
     print ("["+str(font).split("/")[1][:-4]+"] Adding stub DSIG")
@@ -21,24 +28,9 @@ for font in Path("master_ttf").glob("*.ttf"):
     modifiedFont["name"].addMultilingualName({'ja':'Regular'}, modifiedFont, nameID = 2, windows=True, mac=False)
     modifiedFont["head"].flags |= 1 << 3        #sets flag to always round PPEM to integer
 
+    GASP_set(modifiedFont)
     modifiedFont.save("fonts/ttf/"+str(font).split("/")[1])
 
 shutil.rmtree("instance_ufo")
 shutil.rmtree("master_ufo")
 shutil.rmtree("master_ttf")
-
-for font in Path("fonts/ttf/").glob("*.ttf"):
-    print ("["+str(font).split("/")[2][:-4]+"] Autohinting")
-    fontName = str(font)
-    hintedName = fontName[:-4]+"-hinted.ttf"
-    subprocess.check_call(
-        [
-            "ttfautohint",
-            "--stem-width",
-            "nsn",
-            fontName,
-            hintedName,
-        ]
-    )
-
-    shutil.move(hintedName, fontName)
